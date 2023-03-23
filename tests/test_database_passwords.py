30a15426769f09f58a49fdaa5950044eee959a21
Connor Blackler <connor.blackler.ss@gmail.com>
2023-03-23 22:15:32 +0000

    [#3] {t:5} {shared_python:main} [Unit Test] Adding unit test for shared_database/passwords

---
+++ tests/test_database_passwords.py
@@ -0,0 +1,10 @@
+from shared_database.passwords import is_correct_password,hash_password
+import uuid
+
+def test_password_hashing() -> None:
+    for i in range(20):
+        this_password = str(uuid.uuid4())
+        this_salt, this_hashed_password = hash_password(this_password)
+
+        assert is_correct_password(this_salt,this_hashed_password,str(uuid.uuid4())) is False
+        assert is_correct_password(this_salt,this_hashed_password,this_password) is True
