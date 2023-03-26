90a780f9f336c8561489fbed3be4cf2349d39302
Connor Blackler <connor.blackler.ss@gmail.com>
2023-03-26 16:53:40 +0100

    [#5] {t:120} {shared_python} [digest] [hmac] adding shared_hmac, a package to digest data + key to ensure integrity of data

---
+++ tests/test_hmac.py
@@ -0,0 +1,16 @@
+from shared_hmac.hmac import Digestable
+import uuid
+
+def test_digestable() -> None:
+    my_digest_key = "special key"
+    my_digest = Digestable(my_digest_key)
+
+    for i in range(20):
+        special_data = str(uuid.uuid4()).encode("utf-8")
+        data = my_digest.make_digest(special_data)
+
+        assert data == my_digest.make_digest(special_data)
+
+        #Insert a new character to special data
+        altered_data = special_data.decode("utf-8") + "a"
+        assert altered_data.encode("utf-8") != my_digest.make_digest(special_data)
